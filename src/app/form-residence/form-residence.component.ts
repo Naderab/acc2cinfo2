291701c336592dbbfa4d3f554495d71d84fd798e
Naderab <abdellaouinader@gmail.com>
2024-01-17 18:42:12 +0100

    form

--- src/app/form-residence/form-residence.component.ts
+++ src/app/form-residence/form-residence.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-residence',
@@ -8,27 +8,41 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class FormResidenceComponent {
   residence: FormGroup = new FormGroup({
-    name: new FormControl(""),
-    address: new FormControl(''),
-    image:new FormControl('')
+    name: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+    }),
+    parking: new FormControl(''),
+    image: new FormControl(''),
+    category: new FormControl('basic'),
+    appartments: new FormArray([new FormControl('')]),
+    jardin: new FormArray([new FormControl('')]),
   });
   name: FormControl = new FormControl('');
   TestAdd() {
     console.log(this.residence);
     console.log(this.residence.getRawValue());
-    this.residence.patchValue({
+    /*this.residence.patchValue({
       name: 'Test from ts',
       address: 'Test from ts',
-      image:'Test from ts'
-    })
+      image: 'Test from ts',
+    });*/
     console.log(this.residence.getRawValue());
+    console.log(this.appartments);
   }
 
-  ajoutControl() {
+  /**  ajoutControl() {
     this.residence.addControl('test',new FormControl(''));
+  }*/
+  addControl() {
+    this.appartments.push(new FormControl(''));
+  }
+  get appartments() {
+    return this.residence.get('appartments') as FormArray;
   }
 
-  getTest() {
-    return this.residence.get('test');
+  get jardin() {
+    return this.residence.get('jardin') as FormArray;
   }
 }
